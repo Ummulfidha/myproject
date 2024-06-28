9d1465eff12feac32d87c2db6fedd075d1fbbefc
Ummulfidha <114001575+Ummulfidha@users.noreply.github.com>
2024-06-28 20:37:13 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -9,6 +9,7 @@ class addtrainerform(forms.Form):
     tname = forms.CharField(max_length=20)
     tage = forms.IntegerField()
     tphone = forms.CharField(max_length=10)
+    tgender = forms.CharField(max_length=10)
     temail = forms.EmailField(max_length=25)
     tadm = forms.CharField(max_length=34)
 
